c21e048a0a973d8062937eb517294c0a390b6bac
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-11-21 17:03:52 +0000

    Updated forms.py to include new functionality

--- src/base/forms.py
+++ src/base/forms.py
@@ -96,3 +96,11 @@ class RoomForm(forms.ModelForm):
         widgets = {
             'available_from': forms.DateInput(attrs={'type': 'date'})
         }
+
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text'] 
