c5072ab24887e67873b28dc86fed9022e7298db1
diya0603 <diyashetty3000@gmail.com>
2024-11-22 20:55:56 -0500

    added tests for the new functionality

--- src/base/models.py
+++ src/base/models.py
@@ -272,7 +272,7 @@ class ChatRoom(models.Model):
 
 class Message(models.Model):
     room = models.ForeignKey(ChatRoom, related_name='messages', on_delete=models.CASCADE)
-    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
