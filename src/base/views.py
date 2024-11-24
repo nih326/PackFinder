9d78fe5bde687d88b7c9bda4abbcfc8f61cba110
diya0603 <diyashetty3000@gmail.com>
2024-11-24 12:19:32 -0500

    flake8 changes

--- src/base/views.py
+++ src/base/views.py
@@ -35,7 +35,7 @@
 # from django.utils.http import urlsafe_base64_encode
 # from django.template.loader import render_to_string
 # from .filters import ProfileFilter
-from .matching import matchings, calculate_preference_match
+# from .matching import matchings, calculate_preference_match
 
 # from django.contrib.auth import login
 # from django.contrib.auth.models import User
@@ -43,6 +43,7 @@ from .matching import matchings, calculate_preference_match
 # from django.utils.http import urlsafe_base64_decode
 # from base.tokens import account_activation_token
 # from django.views import View
+from .matching import matchings
 from .models import Room
 from .forms import RoomForm
 from django.urls import reverse_lazy
@@ -57,16 +58,12 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import ChatRoom, Message
-from django.contrib.auth import get_user_model
 from django.db.models import Q
-
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.template.loader import render_to_string
 from .filters import ProfileFilter
-from .matching import matchings
-
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.utils.encoding import force_str
@@ -74,152 +71,148 @@ from django.utils.http import urlsafe_base64_decode
 from base.tokens import account_activation_token
 from django.views import View
 
+# class ActivateAccount(View):
+#     """Account activation"""
+#     def get(self, request, uidb64, token, *args, **kwargs):
+#         """GET method for the Account activation."""
+#         try:
+#             uid = force_str(urlsafe_base64_decode(uidb64))
+#             user = get_user_model().objects.get(pk=uid)
+#         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#             user = None
+
+#         if user is not None and account_activation_token.check_token(
+#             user, token
+#         ):
+#             user.is_active = True
+#             user.profile.email_confirmed = True
+#             user.save()
+#             login(request, user)
+#             messages.success(
+#                 request,
+#                 ("Your account has been confirmed. We have logged you in."),
+#             )
+#             return redirect("home")
+#         else:
+#             messages.warning(
+#                 request,
+#                 (
+#                     "The confirmation link was invalid, possibly because it has already been used."
+#                 ),
+#             )
+#             return redirect("home")
 
 
+# class SignUpView(generic.CreateView):
+#     """Sign up View"""
 
-class ActivateAccount(View):
-    """Account activation"""
-    def get(self, request, uidb64, token, *args, **kwargs):
-        """GET method for the Account activation."""
-        try:
-            uid = force_str(urlsafe_base64_decode(uidb64))
-            user = get_user_model().objects.get(pk=uid)
-        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-            user = None
+#     form_class = SignUpForm
+#     success_url = reverse_lazy("login")
+#     template_name = "registration/signup.html"
 
-        if user is not None and account_activation_token.check_token(
-            user, token
-        ):
-            user.is_active = True
-            user.profile.email_confirmed = True
-            user.save()
-            login(request, user)
-            messages.success(
-                request,
-                ("Your account has been confirmed. We have logged you in."),
-            )
-            return redirect("home")
-        else:
-            messages.warning(
-                request,
-                (
-                    "The confirmation link was invalid, possibly because it has already been used."
-                ),
-            )
-            return redirect("home")
+#     def form_valid(self, form):
+#         self.object = form.save(commit=False)
+#         self.object.is_active = False
+#         self.object.save()
 
+#         current_site = get_current_site(self.request)
+#         subject = "Activate Your FindMyRoomie Account"
+#         message = render_to_string(
+#             "emails/account_activation_email.html",
+#             {
+#                 "user": self.object,
+#                 "domain": current_site.domain,
+#                 "uid": urlsafe_base64_encode(force_bytes(self.object.pk)),
+#                 "token": account_activation_token.make_token(self.object),
+#             },
+#         )
+#         self.object.email_user(subject, message)
 
-class SignUpView(generic.CreateView):
-    """Sign up View"""
+#         messages.success(
+#             self.request,
+#             ("Please Confirm your email to complete registration."),
+#         )
 
-    form_class = SignUpForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
-
-    def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.is_active = False
-        self.object.save()
-
-        current_site = get_current_site(self.request)
-        subject = "Activate Your FindMyRoomie Account"
-        message = render_to_string(
-            "emails/account_activation_email.html",
-            {
-                "user": self.object,
-                "domain": current_site.domain,
-                "uid": urlsafe_base64_encode(force_bytes(self.object.pk)),
-                "token": account_activation_token.make_token(self.object),
-            },
-        )
-        self.object.email_user(subject, message)
-
-        messages.success(
-            self.request,
-            ("Please Confirm your email to complete registration."),
-        )
-
-        return HttpResponseRedirect(self.get_success_url())
+#         return HttpResponseRedirect(self.get_success_url())
 
 
-def home(request):
-    """Render Home Page"""
-    user_count = get_user_model().objects.all().count()
-    return render(request, "index.html", {"user_count": user_count})
+# def home(request):
+#     """Render Home Page"""
+#     user_count = get_user_model().objects.all().count()
+#     return render(request, "index.html", {"user_count": user_count})
 
 
-@login_required()
-def profile(request):
-    """Render Profile page"""
-    return render(request, "pages/profile.html", {"profile": request.user.profile})
+# @login_required()
+# def profile(request):
+#     """Render Profile page"""
+#     return render(request, "pages/profile.html", {"profile": request.user.profile})
 
 
-@login_required()
-def profile_edit(request):
-    """Render Edit Profile Page"""
-    profile = Profile.objects.get(user=request.user)
+# @login_required()
+# def profile_edit(request):
+#     """Render Edit Profile Page"""
+#     profile = Profile.objects.get(user=request.user)
 
-    if request.method == "POST":
-        form = ProfileForm(request.POST, request.FILES, instance=profile)
-        if form.is_valid():
-            p = form.save(commit=False)
-            p.is_profile_complete = True
-            print(p.profile_photo)
-            p.save()
+#     if request.method == "POST":
+#         form = ProfileForm(request.POST, request.FILES, instance=profile)
+#         if form.is_valid():
+#             p = form.save(commit=False)
+#             p.is_profile_complete = True
+#             print(p.profile_photo)
+#             p.save()
 
-            return redirect("profile")
+#             return redirect("profile")
 
-    person = Profile.objects.all()
-    form = ProfileForm(instance=profile)
-    return render(
-        request, "pages/profile_edit.html", {"form": form, "profiles": person}
-    )
-
-
-@login_required()
-def findpeople(request):
-    """Render findpeople page"""
-    user_profile = request.user.profile
-    profiles = Profile.objects.exclude(user=request.user)
-
-    if request.GET:
-        user_profile.preference_gender = request.GET.get('gender', user_profile.preference_gender)
-        user_profile.preference_degree = request.GET.get('degree', user_profile.preference_degree)
-        user_profile.preference_course = request.GET.get('course', user_profile.preference_course)
-        user_profile.preference_diet = request.GET.get('diet', user_profile.preference_diet)
-        user_profile.preference_country_id = request.GET.get('country', user_profile.preference_country_id)
-        user_profile.save()
-    # Calculate match percentages
-    profiles_with_match = []
-    for profile in profiles:
-        match_percentage = calculate_preference_match(user_profile, profile)
-        profiles_with_match.append({
-            'profile': profile,
-            'match_percentage': round(match_percentage, 1)
-        })
-    # Sort by match percentage
-    profiles_with_match.sort(key=lambda x: x['match_percentage'], reverse=True)
-    context = {
-        'profiles': profiles_with_match
-    }
-    return render(request, 'pages/findpeople.html', context)
+#     person = Profile.objects.all()
+#     form = ProfileForm(instance=profile)
+#     return render(
+#         request, "pages/profile_edit.html", {"form": form, "profiles": person}
+#     )
 
 
-@login_required
-def myroom(request):
-    """Handle My Room functionality"""
-    profile = request.user.profile
-    # Get rooms where the user has shown interest
-    interested_rooms = Room.objects.filter(interested_users=profile)
-    # Get rooms owned by the user
-    owned_rooms = Room.objects.filter(owner=profile)
-    context = {
-        'interested_rooms': interested_rooms,
-        'owned_rooms': owned_rooms,
-        'profile': profile
-    } 
-    return render(request, "pages/myroom.html", context)
+# @login_required()
+# def findpeople(request):
+#     """Render findpeople page"""
+#     user_profile = request.user.profile
+#     profiles = Profile.objects.exclude(user=request.user)
 
+#     if request.GET:
+#         user_profile.preference_gender = request.GET.get('gender', user_profile.preference_gender)
+#         user_profile.preference_degree = request.GET.get('degree', user_profile.preference_degree)
+#         user_profile.preference_course = request.GET.get('course', user_profile.preference_course)
+#         user_profile.preference_diet = request.GET.get('diet', user_profile.preference_diet)
+#         user_profile.preference_country_id = request.GET.get('country', user_profile.preference_country_id)
+#         user_profile.save()
+#     # Calculate match percentages
+#     profiles_with_match = []
+#     for profile in profiles:
+#         match_percentage = calculate_preference_match(user_profile, profile)
+#         profiles_with_match.append({
+#             'profile': profile,
+#             'match_percentage': round(match_percentage, 1)
+#         })
+#     # Sort by match percentage
+#     profiles_with_match.sort(key=lambda x: x['match_percentage'], reverse=True)
+#     context = {
+#         'profiles': profiles_with_match
+#     }
+#     return render(request, 'pages/findpeople.html', context)
+
+
+# @login_required
+# def myroom(request):
+#     """Handle My Room functionality"""
+#     profile = request.user.profile
+#     # Get rooms where the user has shown interest
+#     interested_rooms = Room.objects.filter(interested_users=profile)
+#     # Get rooms owned by the user
+#     owned_rooms = Room.objects.filter(owner=profile)
+#     context = {
+#         'interested_rooms': interested_rooms,
+#         'owned_rooms': owned_rooms,
+#         'profile': profile
+#     } 
+#     return render(request, "pages/myroom.html", context)
 
 @login_required
 def add_room(request):
@@ -236,25 +229,23 @@ def add_room(request):
     return render(request, 'pages/add_room.html', {'form': form})
 
 
-def user_logout(request):
-    """Log out and redirect to Home Page"""
-    logout(request)
-    messages.success(request, "Logged out successfully!")
-    return redirect("home")
+# def user_logout(request):
+#     """Log out and redirect to Home Page"""
+#     logout(request)
+#     messages.success(request, "Logged out successfully!")
+#     return redirect("home")
 
 @login_required
 def toggle_room_interest(request, room_id):
     try:
         room = Room.objects.get(id=room_id)
         profile = request.user.profile
-        
         if profile in room.interested_users.all():
             room.interested_users.remove(profile)
             messages.success(request, "Removed interest in room.")
         else:
             room.interested_users.add(profile)
             messages.success(request, "Added interest in room.")
-            
         return redirect('myroom')
     except Room.DoesNotExist:
         messages.error(request, "Room not found.")
@@ -291,7 +282,6 @@ class ActivateAccount(View):
             user = get_user_model().objects.get(pk=uid)
         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
             user = None
-
         if user is not None and account_activation_token.check_token(
             user, token
         ):
@@ -316,7 +306,6 @@ class ActivateAccount(View):
 
 class SignUpView(generic.CreateView):
     """Sign up View"""
-
     form_class = SignUpForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
@@ -325,7 +314,6 @@ class SignUpView(generic.CreateView):
         self.object = form.save(commit=False)
         self.object.is_active = False
         self.object.save()
-
         current_site = get_current_site(self.request)
         subject = "Activate Your FindMyRoomie Account"
         message = render_to_string(
@@ -338,12 +326,10 @@ class SignUpView(generic.CreateView):
             },
         )
         self.object.email_user(subject, message)
-
         messages.success(
             self.request,
             ("Please Confirm your email to complete registration."),
         )
-
         return HttpResponseRedirect(self.get_success_url())
 
 
@@ -359,9 +345,7 @@ def profile(request):
     if not request.user.profile.is_profile_complete:
         messages.error(request, "Please complete your profile first!")
         return redirect("profile_edit")
-
     profile = Profile.objects.get(user=request.user)
-
     return render(request, "pages/profile.html", {"profile": profile})
 
 
@@ -369,7 +353,6 @@ def profile(request):
 def profile_edit(request):
     """Render Edit Profile Page"""
     profile = Profile.objects.get(user=request.user)
-
     if request.method == "POST":
         form = ProfileForm(request.POST, request.FILES, instance=profile)
         if form.is_valid():
@@ -377,9 +360,7 @@ def profile_edit(request):
             p.is_profile_complete = True
             print(p.profile_photo)
             p.save()
-
             return redirect("profile")
-
     person = Profile.objects.all()
     form = ProfileForm(instance=profile)
     return render(
@@ -393,7 +374,6 @@ def findpeople(request):
     qs = Profile.objects.filter(visibility=True).exclude(user=request.user)
     f = ProfileFilter(request.GET, queryset=qs)
     user_profile = request.user.profile
-
     if request.GET:
         user_profile.preference_gender = request.GET.get('gender', user_profile.preference_gender)
         user_profile.preference_degree = request.GET.get('degree', user_profile.preference_degree)
