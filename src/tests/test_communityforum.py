6babc252764b1f75f8170f4b995aeedaabecd232
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-11-24 23:33:28 -0500

    Update test_communityforum.py - added new test case

--- src/tests/test_communityforum.py
+++ src/tests/test_communityforum.py
@@ -6,5 +6,13 @@ class ForumPageTests(TestCase):
         """Test that the forum page loads successfully and contains correct links."""
         response = self.client.get(reverse('forum_home'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'forum/forum_home.html')  # Update to 'forum/forum_home.html'
+        self.assertTemplateUsed(response, 'forum/forum_home.html')
         self.assertContains(response, "Community Forum")
+        
+    def test_forum_page_links(self):
+        """Test that all links on the forum page point to the correct URLs."""
+        response = self.client.get(reverse('forum_home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, reverse('housing_tips'))
+        self.assertContains(response, reverse('roommate_issues'))
+        self.assertContains(response, reverse('campus_life'))
