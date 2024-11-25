97fd8d1635599877d30499d09cc60d832034de3a
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-11-24 23:38:36 -0500

    Update test_campuslife.py - new test case

--- src/tests/test_campuslife.py
+++ src/tests/test_campuslife.py
@@ -11,3 +11,9 @@ class CampusLifeTests(TestCase):
         response = self.client.get(reverse('campus_life')) 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forum/campus_life.html')
+
+    def test_campus_life_page_content(self):
+        """Test that the campus life page contains specific text."""
+        response = self.client.get(reverse('campus_life'))
+        self.assertContains(response, "Campus Life: Tips and Discussions")
+        self.assertContains(response, "Discuss all things related to Campus Life here!")
