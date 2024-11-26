a6362693e723283929375bed98b24719221efe1c
sanjana1399 <57448690+sanjana1399@users.noreply.github.com>
2024-11-26 18:05:17 -0500

    Update test_communityforum.py

--- src/tests/test_communityforum.py
+++ src/tests/test_communityforum.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 class ForumPageTests(TestCase):
     def test_forum_page_loads_successfully(self):
         """Test that the forum page loads successfully and contains correct links."""
@@ -8,7 +9,7 @@ class ForumPageTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forum/forum_home.html')
         self.assertContains(response, "Community Forum")
-        
+
     def test_forum_page_links(self):
         """Test that all links on the forum page point to the correct URLs."""
         response = self.client.get(reverse('forum_home'))
